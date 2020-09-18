4f851bbb9b556f63c31b4847687da550e112df45
Shahzaib Waseem <shahzaib.waseem96@gmail.com>
2020-09-18 09:15:01 +0500

    broadcast sending implemented in the SmsBroadcastReceiver.kt after serializing

--- app/src/main/java/com/shahzaib/message/SmsBroadcastReceiver.kt
+++ app/src/main/java/com/shahzaib/message/SmsBroadcastReceiver.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.provider.Telephony
 import android.telephony.SmsMessage
 import android.widget.Toast
+import com.google.gson.Gson
 
 class SmsBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
@@ -14,6 +15,19 @@ class SmsBroadcastReceiver : BroadcastReceiver() {
 
         for (message in messages) {
             Toast.makeText(context, message!!.displayOriginatingAddress + ": " + message.messageBody, Toast.LENGTH_LONG).show()
+
+            val sendBroadcastIntent = Intent()
+            sendBroadcastIntent.action = "Messages"
+            sendBroadcastIntent.putExtra("Message", serialize(Message(message.displayOriginatingAddress, message.messageBody)))
+            context.sendBroadcast(sendBroadcastIntent)
         }
+    }
+
+    fun serialize(obj: Any?): String? {
+        if (obj == null)
+            return null
+
+        val gson = Gson()
+        return gson.toJson(obj)
     }
 }
