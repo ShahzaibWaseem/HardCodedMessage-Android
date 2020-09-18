be38cbcac0dcd706c005804bad661c81742f40b5
Shahzaib Waseem <shahzaib.waseem96@gmail.com>
2020-09-18 09:14:31 +0500

    broadcast receiver implemented in the activity

--- app/src/main/java/com/shahzaib/message/MessageActivity.kt
+++ app/src/main/java/com/shahzaib/message/MessageActivity.kt
@@ -1,5 +1,8 @@
 package com.shahzaib.message
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.os.Bundle
@@ -8,14 +11,30 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
 import com.shahzaib.message.databinding.MessageActivityBinding
 
 class MessageActivity: AppCompatActivity(){
     private val smsManager = SmsManager.getDefault() as SmsManager
     private lateinit var binding: MessageActivityBinding
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var messageAdapter: RecyclerView.Adapter<*>
+    private var messageList: ArrayList<Message> = arrayListOf()
+
     private val SEND_SMS_REQUEST_CODE = 1
     private val RECEIVE_SMS_REQUEST_CODE = 2
-    private val smsReceiver = SmsBroadcastReceiver()
+    
+    private val mBroadcastReceiver = object : BroadcastReceiver()
+    {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val serializedMessage = intent!!.getStringExtra("Message")
+            val message = deserialize(serializedMessage!!, Message::class.java)
+            messageList.add(message)
+            messageAdapter.notifyDataSetChanged()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +42,7 @@ class MessageActivity: AppCompatActivity(){
         val view = binding.root
         setContentView(view)
 
-        val phoneNumber = "ENTER THE PHONE NUMBER HERE"
+        val phoneNumber = "+923075354771"
         val text = "Welcome to C.H.I."
 
         binding.sendingNumberTextView.text = phoneNumber
@@ -38,6 +57,22 @@ class MessageActivity: AppCompatActivity(){
                 ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.SEND_SMS),
                     SEND_SMS_REQUEST_CODE)
         }
+
+        recyclerView = findViewById<RecyclerView>(R.id.messagesRecyclerView).apply {
+            layoutManager = LinearLayoutManager(this@MessageActivity, LinearLayoutManager.VERTICAL, false)
+        }
+
+        val intentFilter = IntentFilter()
+        intentFilter.addAction("Messages")
+        registerReceiver(mBroadcastReceiver, intentFilter)
+
+        messageAdapter = MessagesAdapter(messageList, this@MessageActivity)
+        recyclerView.adapter = messageAdapter
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(mBroadcastReceiver)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -49,5 +84,12 @@ class MessageActivity: AppCompatActivity(){
         else if (requestCode == RECEIVE_SMS_REQUEST_CODE){
             Toast.makeText(this, "Receive Permission Granted", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    fun <T> deserialize(str: String, clazz: Class<T>): T {
+        val gson = Gson()
+        val obj = gson.fromJson(str, clazz)
+
+        return obj as T
     }
 }
